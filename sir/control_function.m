af8b776f4508723ee924364e803bd17d25a79b30
lzino90 <65954970+lzino90@users.noreply.github.com>
2021-05-13 14:54:35 +0200

    Add files via upload

---
+++ sir/control_function.m
@@ -0,0 +1,97 @@
+function [u] = control_function(z,func,t,p)
+%CONTROL_FUNCTION Allows for different control functions
+%INPUT:   func = Vector [The control function case, scaling constant]
+%           z = prevalence of the disease
+%           t = current time step
+%           p = control input up to this point
+%OUTPUT:  u = the control value
+
+global Tcount T1 T2
+
+switch func (1)
+    case 1   %Constant control
+        u = func(2);
+        
+    case 2   %Constant if prevalence above certain threshold
+        if z(t-1) > 0.01
+            u = func(2);
+        else
+            u=0;
+        end
+        
+    case 3   %Constant if prevalence above certain threshold at any given time, with reset
+        
+        if (z(t-1) <= 0.01) && (p(t-1) == 0)
+            u = 0;
+        else
+            u = func(2);
+        end
+        
+    case 4   %Constant if prevalence breaches certain threshold once
+        
+        if max(z) > 0.01 && z(t-1) ~= 0
+            u = func(2);
+        else
+            u = 0;
+        end
+        
+        
+    case 5   %Constant after threshold is reached, and resets if after 14 days there is no new cases.
+        T = 14;
+        if z(t-1) >= 0.01
+            u=func(2);
+        else
+            if t > 20
+                if (sum(z(t-T-1:t-1)) == 0)
+                    u = 0;
+                else
+                    u=func(2);
+                end
+            else
+                u = 0;
+            end
+            
+        end
+        
+        
+    case 6   %Constant for T days after threshold is reached
+        T = 28;
+        if (t == 1) || (t==2)
+            u = 0;
+        elseif z(t-1) >= 0.01 && p(t-2)==0
+            u= func(2);
+            Tcount = t-1;
+        elseif t-Tcount <=T+1
+            u=func(2);
+        else
+            u=0;
+        end
+        
+    case 7   %Constant after threshold is reached, then linear decay
+        T = T1;
+        if (t == 1) || (t==2)
+            u = 0;
+        elseif z(t-1) >= 0.01 && p(t-2)==0
+            u= func(2);
+            Tcount = t-1;
+        elseif t-Tcount <=T+1
+            u=func(2);
+        else
+            u=max(0,p(t-2)-func(2)/T2);
+        end
+
+
+  case 8   %Constant for T=56 days after threshold is reached
+        T = 56;
+        if (t == 1) || (t==2)
+            u = 0;
+        elseif z(t-1) >= 0.01 && p(t-2)==0
+            u= func(2);
+            Tcount = t-1;
+        elseif t-Tcount <=T+1
+            u=func(2);
+        else
+            u=0;
+        end
+end
+end
---
+++ sis/SIS_game.m
@@ -0,0 +1,79 @@
+function [z,zz,zzz,zzzz,u,r] = SIS_game(n,A,m,a, lambda, mu,beta,c,gamma,u_func, r_func,T, init)
+%n: number of nodes
+%A: adjacency matrix influence layer
+%m: number of social interactions
+%a: activity vector (if uniform, a single entry is ok)
+%lambda: per-contact infection probability
+%mu: recovery probability
+%beta: rationality
+%c: cost for self-protective behavior
+%gamma: forgetting factor
+%u_func: control function (see control_function.m)
+%r_func: risk perception function (see risk_function.m)
+%T: simulation time-horizon
+%init: initial fraction of infected nodes, or initial state
+if length(init)==1
+    x=zeros(1,n);
+    x(randperm(n,round(n*init)))=1;
+else
+    x=init;
+end
+y=zeros(1,n);   %Initial self-protection is all zero
+if length(a)==1
+    a=a*ones(1,n);
+end
+A=A*diag(sum(A,2).^(-1));   %Creates a column-stochastic adjacency matrix
+z=zeros(1,T+1);     %The prevalence of the infected
+zz=zeros(1,T+1);    %Proportion of individuals adopting self-protective behaviour
+zzz = zeros(1,T+1); %Accumulation of the cost
+zzzz = zeros(1,T+1); %Accumulation of the frustration
+z(1)=sum(x)/n;
+zz(1)=sum(y)/n;
+f=zeros(1,n);   %Frustration function
+u = zeros(1,T+1);
+for t=2:T+1
+    %% Decision making
+    f=f*gamma+gamma*c*y;   %Accumulation of the frustration
+    r(t-1) = risk_function(z(t-1),r_func);   %Risk perception
+    u(t-1) = control_function(z,u_func,t,u);
+    pi0=(1-y)*A'-u(t-1);                %Payoff for not self-protecting
+    pi1=y*A'+r(t-1)-c-f;         %Payoff for adopting self-protection
+    pp=(exp(beta*pi1))./(exp(beta*pi1)+exp(beta*pi0));  %Probability of self-protection
+    y=double(rand(1,n)<pp);        %Self-protection state update
+    
+    %% Epidemic
+    W=ADN(n,m,a);          %Generate contact network for given time-step
+    ip=(1-(1-lambda).^(W*x'))';    %Probability of individuals being infected
+    %Infected state transition. First term captures transition to I, and
+    %second term recovery to S.
+    x=x+(rand(1,n)<=ip.*(1-x).*(1-y))-(rand(1,n)<mu*x);
+    z(t)=sum(x)/n;
+    zz(t)=sum(y)/n;
+%         zzz(t) = (zzz(t-1)+c*sum(y))./(n*(t-1));
+    zzz(t) = zzz(t-1)+c*sum(y);
+    zzzz(t) = sum(c+f)/n;
+end
+
+
+%% Plotting
+figure
+plot(0:T,zzz)
+xlabel('Time Step, t')
+ylabel('Accumulative Cost')
+
+% figure
+% plot(0:T,zzzz)
+% xlabel('Time Step, t')
+% ylabel('Mean Individual Frustration')
+
+figure(n)
+plot(0:T,z)
+hold on
+plot(0:T,zz)
+% plot(0:T,zzzz)
+legend('Prevalence of Disease','Fraction of Individuals Self-Isolating')
+xlabel('Time Step, t')
+% title('n=5000')
+mean(zz)
+
+end
